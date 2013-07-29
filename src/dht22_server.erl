43453d497be54456a9ab905732954cb67b1a3f9a
Clayton Wheeler <cswh@umich.edu>
2013-07-29 00:52:31 -0400

    Added untested Erlang code to talk to C code.

---
+++ src/dht22_server.erl
@@ -0,0 +1,56 @@
+-module(dht22_server).
+
+-behaviour(gen_server).
+
+-export([start_link/1, get_reading/0]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+start_link(ExtProg) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, ExtProg, []).
+
+init(ExtProg) ->
+    process_flag(trap_exit, true),
+    Port = open_port({spawn, ExtProg}, [{packet, 2}, binary]),
+    {ok, {Port}}.
+
+get_reading() ->
+    gen_server:call(?MODULE, reading).
+
+%% callbacks
+
+handle_call(reading, _From, {Port} = State) ->
+    case port_req_reply(Port, {read, 10}, 20000) of
+        {ok, Res} ->
+            {reply, Res, State};
+        timeout ->
+            {stop, port_timeout, State}
+    end.
+
+handle_info({'EXIT', Port, Reason}, {Port} = State) ->
+    {stop, {port_terminated, Reason}, State}.
+
+terminate({port_terminated, _Reason}, _State) ->
+    ok;
+terminate(_Reason, {Port} = State) ->
+    port_close(Port).
+
+
+port_req_reply(Port, Cmd, Timeout) ->
+    true = port_command(Port, term_to_binary(Cmd)),
+    receive
+        {Port, {data, Data}} ->
+            {ok, binary_to_term(Data)}
+    after Timeout ->
+            timeout
+    end.
+    
+
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
